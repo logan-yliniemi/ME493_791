d92c80baafeb5dfbcbb0ea436778aec79ab808f2
Logan Yliniemi <logan@Logans-MacBook-Pro-2.local>
2017-01-26 09:40:04 -0800

    Intro to C++ File
    File to be modified in class today, January 26 2017, to show functions, classes, pointers, and vectors.

---
+++ intro.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ME493_1
+//
+//  Created by Logan Yliniemi on 1/26/17.
+//  Copyright © 2017 Logan Yliniemi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
